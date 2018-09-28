a896bb10fb1aa57cb40a35f636be7241e46bef3e
Jordan Davis <jtdavis9311@gmail.com>
2018-09-28 11:15:54 -0400

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,99 +1,115 @@
 import React, { Component } from 'react';
-import firebase from './firebase'
+import firebase from './firebase';
+import Navbar from './Navbar';
 
 class App extends Component {
-	constructor(){
-		super()
-		this.state = {}
-		this.handleSubmit = this.handleSubmit.bind(this)
-		this.handleChange = this.handleChange.bind(this)
-		this.loginSubmit = this.loginSubmit.bind(this)
-		this.callNotes = this.callNotes.bind(this)
-	}
+  constructor() {
+    super();
+    this.state = {};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.loginSubmit = this.loginSubmit.bind(this);
+    this.callNotes = this.callNotes.bind(this);
+  }
 
-	callNotes(){
-		const user = firebase.auth().currentUser
-		var ref = firebase.database().ref('notes');
-		let myNotes = []
-		ref.on("value", function(snapshot) {
-			let notes = snapshot.val()
-			for(var key in notes){
-				if(notes[key].author === user.uid){
-					myNotes.push(notes[key].content)
-				}
-			}
-			console.log(myNotes)
-		}, function (error) {
-   	console.log("Error: " + error.code);
-		});
-	}
+  callNotes() {
+    const user = firebase.auth().currentUser;
+    var ref = firebase.database().ref('notes');
+    let myNotes = [];
+    ref.on(
+      'value',
+      function(snapshot) {
+        let notes = snapshot.val();
+        for (var key in notes) {
+          if (notes[key].author === user.uid) {
+            myNotes.push(notes[key].content);
+          }
+        }
+        console.log(myNotes);
+      },
+      function(error) {
+        console.log('Error: ' + error.code);
+      }
+    );
+  }
 
-	handleChange(event){
-		this.setState({
-			[event.target.name]: event.target.value
-		})
-	}
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
 
-	handleSubmit(event){
-		event.preventDefault()
-		const email = this.state.email
-		const pass = this.state.password
-		firebase.auth().createUserWithEmailAndPassword(email, pass)
-			.catch(function(error){
-				console.error(error)
-			})
-	}
+  handleSubmit(event) {
+    event.preventDefault();
+    const email = this.state.email;
+    const pass = this.state.password;
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, pass)
+      .catch(function(error) {
+        console.error(error);
+      });
+  }
 
-	loginSubmit(event){
-		event.preventDefault()
-		const email = this.state.email
-		const pass = this.state.password
-		firebase.auth().signInWithEmailAndPassword(email, pass)
-			.catch(function(error){
-				console.error(error)
-			})
-		const user = firebase.auth().currentUser
-		firebase.database().ref('users/' + user.uid).set({
-			email: user.email
-		})
-		console.log('logged in')
-		this.callNotes()
-	}
+  loginSubmit(event) {
+    event.preventDefault();
+    const email = this.state.email;
+    const pass = this.state.password;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, pass)
+      .catch(function(error) {
+        console.error(error);
+      });
+    const user = firebase.auth().currentUser;
+    firebase
+      .database()
+      .ref('users/' + user.uid)
+      .set({
+        email: user.email,
+      });
+    console.log('logged in');
+    this.callNotes();
+  }
 
-	noteSubmit(event){
-		event.preventDefault()
-		const user = firebase.auth().currentUser
-		firebase.database().ref('notes/note2').set({
-			author: user.uid,
-			content: this.state.note
-		})
-	}
+  noteSubmit(event) {
+    event.preventDefault();
+    const user = firebase.auth().currentUser;
+    firebase
+      .database()
+      .ref('notes/note2')
+      .set({
+        author: user.uid,
+        content: this.state.note,
+      });
+  }
 
   render() {
     return (
       <div className="App">
-				<br />
-				<br />
+        <Navbar />
+        <br />
+        <br />
         <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
-					<input type="email" name="email" />
-					<input type="password" name="password" />
-					<button type="submit">SIGNUP</button>
-				</form>
-				<br />
-				<form onSubmit={this.loginSubmit} onChange={this.handleChange}>
-					<input type="email" name="email" />
-					<input type="password" name="password" />
-					<button type="submit">LOGIN</button>
-				</form>
-				<br />
-				<h3>Post your notes</h3>
-				<form onChange={this.handleChange} onSubmit={this.noteSubmit}>
-					<input type="text" name="note" />
-					<button type="submit">POST NOTE</button>
-				</form>
-				<br />
-				<h3>Read your notes</h3>
-				<p></p>
+          <input type="email" name="email" />
+          <input type="password" name="password" />
+          <button type="submit">SIGNUP</button>
+        </form>
+        <br />
+        <form onSubmit={this.loginSubmit} onChange={this.handleChange}>
+          <input type="email" name="email" />
+          <input type="password" name="password" />
+          <button type="submit">LOGIN</button>
+        </form>
+        <br />
+        <h3>Post your notes</h3>
+        <form onChange={this.handleChange} onSubmit={this.noteSubmit}>
+          <input type="text" name="note" />
+          <button type="submit">POST NOTE</button>
+        </form>
+        <br />
+        <h3>Read your notes</h3>
+        <p />
       </div>
     );
   }
---
+++ src/Navbar.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+//import { mailFolderListItems, otherMailFolderListItems } from './tileData';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 440,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing.unit * 9,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+  icon: {
+    height: '50px',
+  },
+});
+
+class MiniDrawer extends React.Component {
+  render() {
+    const { classes, theme } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="absolute" className={classNames(classes.appBar)}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              className={classNames(classes.menuButton)}
+            />
+            <Typography variant="title" color="inherit" noWrap>
+              FORGET ME NOT
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classNames(classes.drawerPaper),
+          }}
+        >
+          <div className={classes.toolbar}>
+            <IconButton>
+              {theme.direction === 'rtl' ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <img className={classes.icon} src="home.png" />{' '}
+          </List>
+          <Divider />
+          <List>
+            <img className={classes.icon} src="home.png" />
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Typography noWrap />
+        </main>
+      </div>
+    );
+  }
+}
+
+MiniDrawer.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(MiniDrawer);
