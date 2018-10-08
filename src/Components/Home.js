1527393592ff0c56e6b8b965a0ce94aa051d8777
Stella Yun <stellasyyun@gmail.com>
2018-10-08 17:48:05 -0400

    Homepage design

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 import firebase from "../firebase";
+import ReactRevealText from 'react-reveal-text'
 
 class Home extends Component {
   constructor() {
     super();
-    this.state = {};
-    this._mounted = false;
+    this.state = {
+			show: false
+		};
+		this._mounted = false;
   }
 
   componentDidMount() {
+		let message;
+    const date = new Date();
+    const hour = date.getHours();
+    if (hour >= 6 && hour < 12) {
+      message = 'Good morning';
+    } else if (hour >= 12 && hour <= 14) {
+      message = 'Lunch time!';
+    } else if (hour > 14 && hour <= 17) {
+      message = 'Good afternoon';
+    } else if (hour >= 18) {
+      message = 'Good night';
+    }
+    this.setState({ message });
+		setTimeout(() => {
+      this.setState({ show: true });
+    }, 1000);
     this._mounted = true;
     const self = this;
     const ref = firebase.database().ref("users");
@@ -30,14 +49,17 @@ class Home extends Component {
 
   componentWillMount() {
     this._mounted = false;
-  }
+	}
 
   render() {
-    const name = this.state.userName;
+		const name = this.state.userName + '';
+		const message = this.state.message + '';
     return (
       <div>
-        <h1>HOME</h1>
-        <p>{name ? name : null}</p>
+				<div>
+					<ReactRevealText style={{fontSize: '4em'}} show={this.state.show}>{message}</ReactRevealText>
+					<ReactRevealText style={{fontSize: '3em'}} show={this.state.show}>{name}</ReactRevealText>
+				</div>
       </div>
     );
   }
