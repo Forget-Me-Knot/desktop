8b900de5beb7f9eb832eef7c5be286845baae73f
Katharine Francis <katie-francis@hotmail.com>
2018-10-08 10:14:24 -0400

    Worked on Member

--- src/Components/Members.js
+++ src/Components/Members.js
@@ -11,8 +11,12 @@ class Members extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      members: []
+      members: [],
+      users: [],
+      projects: [],
+      formOpen: false
     };
+    this.openForm = this.openForm.bind(this);
     this.delete = this.delete.bind(this);
   }
 
@@ -26,16 +30,26 @@ class Members extends React.Component {
           const users = snapshot.val().users;
 
           let myMembers = [];
+          let newMembers = [];
 
           for (var id in users) {
             projects.map(project => {
               if (project.members.includes(users[id].email)) {
-                myMembers.push(users[id].displayName);
+                myMembers.push({
+                  displayName: users[id].displayName,
+                  email: users[id].email
+                });
                 console.log("Members: ", myMembers);
+              } else {
+                newMembers.push({
+                  displayName: users[id].displayName,
+                  email: users[id].email
+                });
+                console.log("New members: ", newMembers);
               }
             });
           }
-          self.setState({ members: myMembers });
+          self.setState({ members: myMembers, users: newMembers });
         });
       }
     });
@@ -51,16 +65,27 @@ class Members extends React.Component {
           ref.on("value", function(snapshot) {
             const users = snapshot.val().users;
             let myMembers = [];
+            let newMembers = [];
             for (var id in users) {
               props.projects.map(project => {
                 if (project.members.includes(users[id].email)) {
-                  myMembers.push(users[id].displayName);
+                  myMembers.push({
+                    displayName: users[id].displayName,
+                    email: users[id].email
+                  });
                   console.log("Members: ", myMembers);
+                } else {
+                  newMembers.push({
+                    displayName: users[id].displayName,
+                    email: users[id].email
+                  });
+                  console.log("New members: ", newMembers);
                 }
               });
             }
             self.setState({
               projects: props.projects,
+              users: newMembers,
               members: myMembers
             });
           });
@@ -68,6 +93,15 @@ class Members extends React.Component {
       });
     }
   }
+
+  openForm() {
+    if (!this.state.formOpen) {
+      this.setState({ formOpen: true });
+    } else {
+      this.setState({ formOpen: false });
+    }
+  }
+
   delete(key) {
     return firebase
       .database()
@@ -77,27 +111,38 @@ class Members extends React.Component {
   }
 
   memberList(members) {
-    return members.map(member => (
-      <ListItem key={member.key}>
-        <ListItemText>{member}</ListItemText>
-        <IconButton
-          aria-label="Delete"
-          color="grey"
-          style={{ float: "right" }}
-          onClick={() => this.delete(member.key)}
-        >
-          <RemoveCircle />
-        </IconButton>
-      </ListItem>
-    ));
+    if (members) {
+      return members.map(member => (
+        <ListItem key={member.key}>
+          <ListItemText>{member.displayName}</ListItemText>
+          <ListItemText>{member.email}</ListItemText>
+          <IconButton
+            aria-label="Delete"
+            color="grey"
+            style={{ float: "right" }}
+            onClick={() => this.delete(member.key)}
+          >
+            <RemoveCircle />
+          </IconButton>
+        </ListItem>
+      ));
+    }
   }
 
   render() {
     const members = this.state.members;
+    console.log("Members Array: ", members);
     return (
       <div>
-        <List>{members ? this.memberList(members) : null}</List>
-        <AddMember project={this.props.projects} />
+        <List>
+          {this.state.members && this.state.members.displayName
+            ? this.memberList(this.state.members)
+            : null}
+        </List>
+        <AddMember
+          project={this.props.projects}
+          newMembers={this.state.users}
+        />
       </div>
     );
   }
